d06b953be384b29d72f0dae9ac0629ba2430ade6
ajb85 <ajbrush85@gmail.com>
2019-04-12 11:34:25 -0400

    Route for /games post created

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
+
+server.post("/games", async ({ body: { title, genre, releaseYear } }, res) => {
+  try {
+    if (title && genre) {
+      res.status(201).json({ message: "OK" });
+    } else {
+      res.status(422).json({ message: "Please supply title & genre" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err });
+  }
+});
--- server.spec.js
+++ server.spec.js
@@ -7,24 +7,24 @@ describe("/games posts", () => {
     genre: "Arcade",
     releaseYear: 1980
   };
-  const badData = { title: "Pacman" };
 
   it("should return 201 for valid requests", async () => {
-    const res = request(server)
+    const res = await request(server)
       .post("/games")
       .send(goodData);
     expect(res.status).toBe(201);
   });
 
   it("should return json", async () => {
-    const res = request(server)
+    const res = await request(server)
       .post("/games")
       .send(goodData);
     expect(res.type).toBe("application/json");
   });
 
   it("should return 422 for valid requests", async () => {
-    const res = request(server)
+    const badData = { title: "Pacman" };
+    const res = await request(server)
       .post("/games")
       .send(badData);
     expect(res.status).toBe(422);
